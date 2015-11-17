91fb340c5f68cd764378df44d6e7ae66cb391d88
uicab2593 <uicab2593@gmail.com>
2015-11-17 17:05:40 -0500

    sd

--- readingPort.php
+++ readingPort.php
@@ -3,7 +3,7 @@ require 'vendor/autoload.php';
 
 $serial = new PhpSerial;
 
-$serial->deviceSet("/dev/ttyACM1");
+$serial->deviceSet("/dev/ttyACM2");
 
 $serial->confBaudRate(57600);
 $serial->confParity("none");
@@ -16,20 +16,53 @@ $context = new ZMQContext();
 $socket = $context->getSocket(ZMQ::SOCKET_PUSH,'my pusher');
 $socket->connect("tcp://127.0.0.1:5555");
 /*new image*/
+$new = "";
+$MASK = 255;
+$MASK2 = $MASK<<8;
+$image = fopen("test2","w");
+$lastFrame=0;
+// default str
+$defaultStr = getDefaultStr();
+// 
 while(true){
-	while(($read = $serial->readPort()) == "");
-	$image = fopen("test2","w");
-	$socket->send(1);
-	fwrite($image,$read);	
-	$time = time();
-	while(time() - $time < 3){
-		while(($read = $serial->readPort()) == "" && time() - $time < 3);
-		if(time() - $time < 3){
-			fwrite($image,$read);
-			$time = time();
+	$read = "";
+	while(strlen($read .= $serial->readPort()) < 32);
+	$dataStr = substr($read,0,32);
+	$data = unpack('C*',$dataStr);
+	$read = substr($read,count($data));
+	$nFrames = (($data[1]<<8) & $MASK2) + $data[2];
+	$frame = (($data[3]<<8) & $MASK2) + $data[4];
+	if($frame==1){
+		rewind($image);
+		ftruncate($image,0);
+		$socket->send(1);
+	}
+
+	if($frame==$lastFrame+1){
+		fwrite($image,substr($dataStr,4));
+		$lastFrame+=1;
+	}
+	else{
+		// si ocurren un error en los datos, llegan numeros muy grandes. tuyo?
+		if($frame>$lastFrame+100 || $frame<$lastFrame-100){
+			fwrite($image,$defaultStr);
+			$lastFrame+=1;
+		}else{
+			fwrite($image,getDefaultStr((($frame-$lastFrame)-1)*28));
+			fwrite($image,substr($dataStr,4));
+			$lastFrame = $frame;
 		}
 	}
-	$socket->send(2);
-	fclose($image);
+	echo $frame."\n";
+	echo $nFrames."\n";
+	echo "\n";
+	if($frame==$nFrames) $socket->send(2);
 }
 $serial->deviceClose();
+
+function getDefaultStr($bytes=28){
+	$defaultStr = '';
+	for ($i=0; $i < $bytes; $i++) { 
+		$defaultStr.=chr(0);
+	}	
+}
--- sendImage.php
+++ sendImage.php
@@ -9,14 +9,23 @@ $serial->confParity("none");
 $serial->confCharacterLength(32);
 $serial->confStopBits(1);
 $serial->confFlowControl("none");
-$source = file_get_contents($_FILES['image']['tmp_name']);
+
+$source = file_get_contents(isset($_FILES['image']['tmp_name'])?$_FILES['image']['tmp_name']:'descarga.png');
 $serial->deviceOpen();
 $index=0;
 $len = strlen($source);
+$nFrames = ceil($len/28); 
+$curFrame = 1;
+$start = true;
+$MASK = 255;
+echo $nFrames."\n";
 while($index<$len){
-	$aux = substr($source,$index,32);
+	$aux = substr($source,$index,28);
+	$aux = chr(($nFrames>>8) & $MASK).chr($nFrames & $MASK).chr(($curFrame>>8) & $MASK).chr($curFrame & $MASK).$aux;
+	echo $curFrame."\n";
 	$serial->sendMessage($aux);
 	while($serial->readPort()=="");
-	$index += strlen($aux);
+	$index += 28;
+	$curFrame++;
 }
 $serial->deviceClose();
